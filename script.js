8a69060b3953860bb332ddd54b2640cdd427ef8a
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-20 16:56:48 -0700

    changed classes on HTML and defined some variables

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var today = moment ();
+
+var timeEl = document.querySelector(".container")
+
+$('#currentDay').text(today.format('LLLL'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
