8ceef8649d9eabc2aee1878d527b13e5dabe9c89
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-20 17:45:28 -0700

    updated function

--- script.js
+++ script.js
@@ -1,57 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// var today = moment ();
-
-// var timeEl = document.querySelector(".container")
-
-// $('#currentDay').text(today.format('LLLL'));
-
-// $('.saveBtn').on('click', function () {
-//   var text = $(this).siblings('.description').val();
-
-//   var time = $(this).parent().attr('id');
-
-//   localStorage.setItem(text, time);
-// });
-
-// $('#hour-9 .description').val(localStorage).getItem('hour-9');
-// $('#hour-10 .description').val(localStorage).getItem('hour-10');
-// $('#hour-11 .description').val(localStorage).getItem('hour-11');
-// $('#hour-12 .description').val(localStorage).getItem('hour-12');
-// $('#hour-13 .description').val(localStorage).getItem('hour-13');
-// $('#hour-14 .description').val(localStorage).getItem('hour-14');
-// $('#hour-15 .description').val(localStorage).getItem('hour-15');
-// $('#hour-16 .description').val(localStorage).getItem('hour-16');
-// $('#hour-17 .description').val(localStorage).getItem('hour-17');
-
-
-// function colorChange () {
-
-//   var currentHour = today.hours();
-
-//   $('.time-block').each(function () {
-//     var timeId = parseInt($(this).attr('id').split('hour')[1]);
-
-//     if (timeId < currentHour) {
-//       $(this).addClass('past');
-//     } else if (timeId === currentHour) {
-//       $(this).removeClass('past');
-//       $(this).removeClass('future');
-//       $(this).addClass('present');
-//     } else {
-//       $(this).removeClass('past');
-//       $(this).removeClass('present');
-//       $(this).addClass('future');
-//     }
-    
-//   });
-// }
-// colorChange();
-
-// setTimeout(function () {
-//   location = '';
-//   }, 1000 * 60)
  var date = $('#dateTime');
 
  var saveBtn = document.querySelector('.saveBtn');
@@ -63,7 +9,21 @@
   dateAndTime.text(displayedDate);
  }
 
- 
+ function hourTracker() {
+  $('.schedule').each(function () {
+    var futureHour = $(this).attr('id').split('time')[1];
+    console.log('currentHour ' + currentHour);
+    console.log('futureHour ' + futureHour);
+
+    if (futureHour < currentHour) {
+      $(this).addClass('past');
+    } else if (futureHour == currentHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  })
+ }
   $('.saveBtn').on('click', function () {
     var text = $(this).siblings('.description').val();
   
