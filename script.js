353802f3c2ed1dd967b026b18806949f219981f0
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-20 17:18:18 -0700

    added function to determine color for hour

--- script.js
+++ script.js
@@ -16,17 +16,42 @@ $('.saveBtn').on('click', function () {
 });
 
 $('#hour-9 .description').val(localStorage).getItem('hour-9');
-$('#hour-10 .description').val(localStorage).getItem('hour-10')
-$('#hour-11 .description').val(localStorage).getItem('hour-11')
-$('#hour-12 .description').val(localStorage).getItem('hour-12')
-$('#hour-13 .description').val(localStorage).getItem('hour-13')
-$('#hour-14 .description').val(localStorage).getItem('hour-14')
-$('#hour-15 .description').val(localStorage).getItem('hour-15')
-$('#hour-16 .description').val(localStorage).getItem('hour-16')
-$('#hour-17 .description').val(localStorage).getItem('hour-17')
+$('#hour-10 .description').val(localStorage).getItem('hour-10');
+$('#hour-11 .description').val(localStorage).getItem('hour-11');
+$('#hour-12 .description').val(localStorage).getItem('hour-12');
+$('#hour-13 .description').val(localStorage).getItem('hour-13');
+$('#hour-14 .description').val(localStorage).getItem('hour-14');
+$('#hour-15 .description').val(localStorage).getItem('hour-15');
+$('#hour-16 .description').val(localStorage).getItem('hour-16');
+$('#hour-17 .description').val(localStorage).getItem('hour-17');
 
 
-$(function () {
+function colorChange () {
+
+  var currentHour = today.hours();
+
+  $('.time-block').each(function () {
+    var timeId = parseInt($(this).attr('id').split('hour')[1]);
+
+    if (timeId < currentHour) {
+      $(this).addClass('past');
+    } else if (timeId === currentHour) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    } else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+    
+  });
+}
+colorChange();
+
+setTimeout(function () {
+  location = '';
+  }, 1000 * 60)
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -45,4 +70,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
