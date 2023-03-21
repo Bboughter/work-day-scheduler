15e7f1a3e45625d32725f1d0ea2a5e6d420f46f6
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-20 17:04:56 -0700

    added local storage grab in JavaScript

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ var timeEl = document.querySelector(".container")
 
 $('#currentDay').text(today.format('LLLL'));
 
+$('.saveBtn').on('click', function () {
+  var text = $(this).siblings('.description').val();
+
+  var time = $(this).parent().attr('id');
+
+  localStorage.setItem(text, time);
+});
+
+$('#hour-9 .description').val(localStorage).getItem('hour-9');
+$('#hour-10 .description').val(localStorage).getItem('hour-10')
+$('#hour-11 .description').val(localStorage).getItem('hour-11')
+$('#hour-12 .description').val(localStorage).getItem('hour-12')
+$('#hour-13 .description').val(localStorage).getItem('hour-13')
+$('#hour-14 .description').val(localStorage).getItem('hour-14')
+$('#hour-15 .description').val(localStorage).getItem('hour-15')
+$('#hour-16 .description').val(localStorage).getItem('hour-16')
+$('#hour-17 .description').val(localStorage).getItem('hour-17')
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
